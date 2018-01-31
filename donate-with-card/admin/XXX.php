fb6623ebb6ed5a3019399d856a2ac11d1f67ad81
Yusuf Gündüz <Kambaa@users.noreply.github.com>
2018-01-31 11:45:08 +0300

    Create XXX.php
    an idea to make it easy for needing to prefix every public method for wordpress.

---
+++ donate-with-card/admin/XXX.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yg
+ * Date: 1/31/18
+ * Time: 10:58 AM
+ */
+
+class XXX
+{
+    const PREFIX = 'dwc_';
+
+    public function __construct()
+    {
+
+    }
+
+
+    /**
+     * Instead of putting prefixes for every method name that needs for wordpress, this method strips prefix,
+     * searches in public methods and if there's one, calls the method.
+     * @param $name string undefined function name. It should start with the value defined in the PREFIX constant.
+     * @param $arguments mixed method arguments to run.
+     */
+    public function __call($name, $arguments)
+    {
+        if (strpos($name, self::PREFIX) === 0) {
+            $realMethodName = substr($name, 4);
+            if (self::isEligibleForMethodCall($realMethodName)) {
+                call_user_func_array(array($this, $realMethodName), $arguments);
+            } else {
+                exit("Method `$name` not found!");
+            }
+        } else {
+            exit("Method `$name` not found!");
+        }
+    }
+
+    /**
+     * Searches in public methods and returns if given method is public, but not static.
+     * @param $name string
+     * @return bool
+     */
+    public static function isEligibleForMethodCall($name)
+    {
+        $result = false;
+        try {
+            $reflection = new \ReflectionClass(__CLASS__);
+            $methods = $reflection->getMethods(ReflectionMethod::IS_PUBLIC);
+            if (is_array($methods)) {
+                foreach ($methods as $method) {
+                    if ($method->name == $name && !$method->isStatic() && $method->isPublic()) {
+                        $result = true;
+                        break;
+                    }
+                }
+            }
+        } catch (Exception $e) {
+            $result = false;
+        } finally {
+            return $result;
+        }
+    }
+
+
+    public function anan($name)
+    {
+        echo 'senin ben ' . $name;
+    }
+
+}
+
+
+$x = new XXX();
+$x->dwc_anan("wasd");
