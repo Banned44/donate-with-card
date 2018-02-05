2465f075b23d76da9d21a76709b8f5bbdeccadee
Yusuf Gündüz <Kambaa@users.noreply.github.com>
2018-02-05 16:47:39 +0300

    Update donate-with-card.php

--- donate-with-card/donate-with-card.php
+++ donate-with-card/donate-with-card.php
@@ -104,11 +104,11 @@ function dwc_deactivate()
 
 function dwc_uninstall()
 {
-    global $wpdb;
-    $wpdb->query("DROP TABLE IF EXISTS $donationTypesTableName;");
-    $wpdb->query("DROP TABLE IF EXISTS $donationsTableName;");
-    $wpdb->query("DROP TABLE IF EXISTS $donationItemsTableName;");
-    delete_option('donate_with_card_db_version');
+//    global $wpdb;
+//    $wpdb->query("DROP TABLE IF EXISTS $donationTypesTableName;");
+//    $wpdb->query("DROP TABLE IF EXISTS $donationsTableName;");
+//    $wpdb->query("DROP TABLE IF EXISTS $donationItemsTableName;");
+//    delete_option('donate_with_card_db_version');
 }
 
 register_activation_hook(__FILE__, "dwc_ddl");
@@ -117,170 +117,9 @@ register_deactivation_hook(__FILE__, "dwc_deactivate");
 // register_uninstall_hook(__FILE__,"dwc_uninstall");
 register_deactivation_hook(__FILE__, "dwc_uninstall");
 
-
-// function wpdocs_register_my_custom_menu_page() {
-// // add_menu_page('My Custom Page', 'My Custom Page', 'manage_options', 'my-top-level-slug');
-// // add_submenu_page( 'my-top-level-slug', 'My Custom Page', 'My Custom Page','manage_options', 'my-top-level-slug');
-// // add_submenu_page( 'my-top-level-slug', 'My Custom Submenu Page', 'My Custom Submenu Page',    'manage_options', 'my-secondary-slug');
-
-//     add_submenu_page(
-//         'tools.php',
-//         'My Custom Submenu Pagwasdasde',
-//         'Donate With Card Options',
-//         'manage_options',
-//         'my-custom-submenu-page',
-//         'wpdocs_my_custom_submenu_page_callback' );
-// }
-// function wpdocs_my_custom_submenu_page_callback() {
-//     echo '<div class="wrap"><div id="icon-tools" class="icon32"></div>';
-//         echo '<h2>My Custom Submenu Page</h2>';
-//     echo '</div>';
-// }
-// add_action( 'admin_menu', 'wpdocs_register_my_custom_menu_page' );
-
-class DonateWithCard
-{
-
-    public function addAdminPage()
-    {
-        // Add the menu item and page
-        $page_title = 'Donate With Card - Settings - Donation Types';
-        $menu_title = 'Donation Types';
-        $capability = 'manage_options';
-        $slug = 'donate-with-card-settings-donation-types';
-        $callback = "dwcDonationTypesDisplay";//array( $this, 'plugin_settings_page_content' );
-        $icon = 'dashicons-admin-plugins';
-        $position = 100;
-        add_menu_page($page_title, $menu_title, $capability, $slug, $callback, $icon, $position);
-
-//        add_submenu_page('options-general.php', $page_title, $menu_title, $capability, $slug, $callback);
-    }
-
-
-    public function __construct()
-    {
-        // Hook into the admin menu
-        add_action('admin_menu', array($this, 'addAdminPage'));
-    }
-
-    public function plugin_settings_page_content()
-    {
-        // echo 'Hello World!';
-        if ($_POST['updated'] === 'true') {
-            $this->handle_form();
-        }
-        ?>
-        <div class="wrap">
-            <h2>My Awesome Settings Page</h2>
-            <form method="POST">
-                <input type="hidden" name="updated" value="true"/>
-                <?php wp_nonce_field('awesome_update', 'awesome_form'); ?>
-                <table class="form-table">
-                    <tbody>
-                    <tr>
-                        <th><label for="username">Username</label></th>
-                        <td><input name="username" id="username" type="text"
-                                   value="<?php echo get_option('awesome_username'); ?>" class="regular-text"/></td>
-                    </tr>
-                    <tr>
-                        <th><label for="email">Email Address</label></th>
-                        <td><input name="email" id="email" type="text"
-                                   value="<?php echo get_option('awesome_email'); ?>" class="regular-text"/></td>
-                    </tr>
-                    </tbody>
-                </table>
-                <p class="submit">
-                    <input type="submit" name="submit" id="submit" class="button button-primary" value="Check My Info!">
-                </p>
-            </form>
-        </div> <?php
-    }
-
-    public function handle_form()
-    {
-        if (
-            !isset($_POST['awesome_form']) ||
-            !wp_verify_nonce($_POST['awesome_form'], 'awesome_update')
-        ) { ?>
-            <div class="error">
-                <p>Sorry, your nonce was not correct. Please try again.</p>
-            </div> <?php
-            exit;
-        } else {
-            $valid_usernames = array('admin', 'matthew');
-            $valid_emails = array('email@domain.com', 'anotheremail@domain.com');
-
-            $username = sanitize_text_field($_POST['username']);
-            $email = sanitize_email($_POST['email']);
-
-            if (in_array($username, $valid_usernames) && in_array($email, $valid_emails)) {
-                update_option('awesome_username', $username);
-                update_option('awesome_email', $email); ?>
-                <div class="updated">
-                    <p>Your fields were saved!</p>
-                </div> <?php
-            } else { ?>
-                <div class="error">
-                    <p>Your username or email were invalid.</p>
-                </div> <?php
-            }
-        }
-    }
-}
-
-new DonateWithCard();
-
-//include( plugin_dir_path( __FILE__ ) . 'admin/donation-types.php');
-
-
-function dwcDonationTypesDisplay()
-{
-    global $wpdb;
-    $sql = "SELECT * FROM `" . $wpdb->prefix . "donation-types` " . "ORDER BY ord ASC";
-    $result = $wpdb->get_results($sql, ARRAY_A);
-
-    ?>
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css" type="text/css"
-          media="all"/>
-    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-    <script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
-
-
-    <h2>Donation Types</h2>
-    <table id="example" class="display" cellspacing="0" width="100%">
-        <thead>
-        <tr>
-            <th>#</th>
-            <th>Name</th>
-            <th>Label</th>
-            <th>Default Price</th>
-            <th>Edit</th>
-        </tr>
-        </thead>
-        <tbody>
-        <?php
-        foreach ($result as $v) {
-            $v['default_price'] = empty($v['default_price']) ? "-" : $v['default_price'];
-            echo "<tr><td>{$v['id']}</td><td>{$v['name']}</td><td>{$v['label']}</td><td>{$v['default_price']}</td><td><a href='#{$v['id']}'><span class=\"dashicons dashicons-edit\"></span></a></td>";
-        }
-        ?>
-        </tbody>
-    </table>
-
-    <script>
-        $(document).ready(function () {
-            $('#example').DataTable({
-                responsive: {
-                    details: false
-                },
-                "lengthMenu": [[10, 25, 50, -1], [10, 25, 50, "All"]]
-            });
-        });
-    </script>
-    <?php
-
-}
-
+require plugin_dir_path(__FILE__) . "admin/PluginBase.php";
+require plugin_dir_path(__FILE__) . "admin/dt.php";
+new Dt();
 
 // MENU STRUCTURE
 // DonateWithCard
@@ -294,29 +133,10 @@ function dwcDonationTypesDisplay()
 
 function my_plugin_load_plugin_textdomain()
 {
-    load_plugin_textdomain( 'dwc-plugin', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+    load_plugin_textdomain('dwc-plugin', false, dirname(plugin_basename(__FILE__)) . '/languages/');
 }
 
 add_action('plugins_loaded', 'my_plugin_load_plugin_textdomain');
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 function my_registration_form($params, $content = null)
@@ -419,7 +239,7 @@ function my_registration_form($params, $content = null)
     </form>
     <script>
         $ = jQuery;
-
+        var adminUrl = "<?php echo admin_url('admin-ajax.php'); ?>";
         $(function () {
 
             var dt =<?php echo json_encode($donationTypes);?>;
@@ -466,6 +286,30 @@ function my_registration_form($params, $content = null)
                     donationCart.push(data);
                     resetDonationTypeFields();
                     displayCart();
+
+                    var data = {
+                        action: 'my_first_ajax_action',
+                        post_var: 'this will be echoed back',
+                        wasd: "wasdsad"
+                    };
+                    // // the_ajax_script.ajaxurl is a variable that will contain the url to the ajax processing file
+                    // $.post(the_ajax_script.ajaxurl, data, function (response) {
+                    //     alert(response);
+                    // });
+
+                    $.ajax({
+                            "url": the_ajax_script.ajaxurl,
+                            "type": "post",
+                            "data": data,
+                            success: function (obj) {
+                                console.log("success!");
+                                console.log(obj);
+                            },
+                            error: function (obj) {
+                                console.log("error! status:" + obj.status + "\n" + "text:" + obj.responseText);
+                            }
+                        }
+                    );
                 }
             }
 
@@ -547,8 +391,6 @@ function my_registration_form($params, $content = null)
                 },
                 container: '.card-wrapper'
             });
-
-
         });
 
     </script>
@@ -559,4 +401,25 @@ function my_registration_form($params, $content = null)
 add_shortcode('my_form', 'my_registration_form');
 
 
+function my_ajax_callback_function()
+{
+    echo "hiii";
+    print_r($_POST);
+    wp_die();
+}
+
+add_action('wp_ajax_my_first_ajax_action', 'my_ajax_callback_function');
+add_action('wp_ajax_nopriv_my_first_ajax_action', 'my_ajax_callback_function');
+
+
+function test_ajax_load_scripts()
+{
+    // load our jquery file that sends the $.post request
+    wp_enqueue_script("ajax-test", plugin_dir_url(__FILE__) . '/ajax-test.js', array('jquery'));
+
+    // make the ajaxurl var available to the above script
+    wp_localize_script('ajax-test', 'the_ajax_script', array('ajaxurl' => admin_url('admin-ajax.php')));
+}
+
+add_action('wp_print_scripts', 'test_ajax_load_scripts');
 ?>
