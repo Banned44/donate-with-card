57767b70fc65ea0450cbdbc9793f6428fd3b1bdc
Yusuf Gündüz <yusuf.gunduz@epias.com.tr>
2018-02-06 11:35:41 +0300

    label update

--- public/includes/donate_form_view.php
+++ public/includes/donate_form_view.php
@@ -2,87 +2,59 @@
     var adminUrl = "<?php echo admin_url('admin-ajax.php'); ?>";
     var dt =<?php echo json_encode($donationTypes);?>;
 </script>
-<h3>Bağış Seçiniz</h3>
+<h3><?php _e("Select Donation", "dwc-plugin"); ?></h3>
 <form>
     <select id="donation_types" style="width:75%;float:left;">
-        <option value="-1">Bağış Türü Seçiniz</option>
+        <option value="-1"><?php _e("Select donation type", "dwc-plugin"); ?></option>
     </select>
     <input id="donation_amount" type="number" minlength="0" min="0" style="margin-left:2%;width:23%;float:left;"/>
-    <input type="button" id="addToCart" value="Kutuya Ekle" style="clear:both;margin-top:12px;"/>
+    <input type="button" id="addToCart" value="<?php _e("Add to box", "dwc-plugin"); ?>"
+           style="clear:both;margin-top:12px;"/>
 </form>
-<h3>Bağış Kutusu</h3>
+<h3><?php _e("Donation Box", "dwc-plugin"); ?></h3>
 <div id="cartContainer" style="padding-left: 20px;">
     <ul id="donationCart">
         <hr>
-        <li>Toplam <span style="float:right;">0 TL</span></li>
+        <li><?php _e("Total", "dwc-plugin"); ?> <span style="float:right;">0 TL</span></li>
     </ul>
 </div>
-<h3>Bilgileriniz</h3>
+<h3><?php _e("Your Info", "dwc-plugin"); ?></h3>
 <form id="donation_infos" method="post" action="">
 
-    <label for="">Adınız Soyadınız
+    <label for=""><?php _e("Your name", "dwc-plugin"); ?>
         <input type="text" name="name"/>
     </label>
 
-    <label for="">Telefon Numaranız
+    <label for=""><?php _e("Your Phone", "dwc-plugin"); ?>
         <input type="tel" name="tel"/>
     </label>
 
-    <label for="">E-Posta Adresiniz
+    <label for=""><?php _e("Your E-mail", "dwc-plugin"); ?>
         <input type="email" name="email"/>
     </label>
 
-    <label for="">T.C. Kimlik Numaranız
+    <label for=""><?php _e("Your SSN", "dwc-plugin"); ?>
         <input type="text" name="tckn"/>
     </label>
 
-    <label for="">Bağış Notunuz
+    <label for=""><?php _e("Notes", "dwc-plugin"); ?>
         <textarea name="donation_notes" rows="5" cols="30"></textarea>
     </label>
 
     <div class='card-wrapper'></div>
 
-    <input type="text" id="cardholder_name" name="name" placeholder="Kart sahibi adı"
+    <input type="text" id="cardholder_name" name="name" placeholder="<?php _e("Cardholder Name", "dwc-plugin"); ?>"
            style="width:50%;float:left;"/>
-    <input type="text" id="card_number" name="number" placeholder="Kart no" style="width:50%;float:left;">
+    <input type="text" id="card_number" name="number" placeholder="<?php _e("Card No", "dwc-plugin"); ?>"
+           style="width:50%;float:left;">
     <div style="clear:both;"></div>
     <div style="width:50%;float:left;">
-        <select name="month" style="width:50%;float:left;">
-            <option value="">Ay Seçiniz</option>
-            <option value="01">Ocak</option>
-            <option value="02">Şubat</option>
-            <option value="03">Mart</option>
-            <option value="04">Nisan</option>
-            <option value="05">Mayıs</option>
-            <option value="06">Haziran</option>
-            <option value="07">Temmuz</option>
-            <option value="08">Ağustos</option>
-            <option value="09">Eylül</option>
-            <option value="10">Ekim</option>
-            <option value="11">Kasım</option>
-            <option value="12">Aralık</option>
-        </select>
-        <select name="year" style="width:50%;float:left;">
-            <option value="">Yıl Seçiniz</option>
-            <option>2018</option>
-            <option>2019</option>
-            <option>2020</option>
-            <option>2021</option>
-            <option>2022</option>
-            <option>2023</option>
-            <option>2024</option>
-            <option>2025</option>
-            <option>2026</option>
-            <option>2027</option>
-            <option>2028</option>
-            <option>2029</option>
-            <option>2030</option>
-        </select>
+        <input type="text" name="expiry" placeholder="<?php _e("Expires","dwc-plugin");?>"
     </div>
     <div style="width:50%;float:left;">
-        <input type="text" id="card_cvc" name="cvc" placeholder="CVC Kodu"/>
+        <input type="text" id="card_cvc" name="cvc" placeholder="<?php _e("CVC Code", "dwc-plugin"); ?>"/>
     </div>
     <div style="clear:both;"></div>
-    <input type="submit" name="donation" value="Bağış Yapın"/>
+    <input type="submit" name="donation" value="<?php _e("Donate", "dwc-plugin"); ?>"/>
 
 </form>
