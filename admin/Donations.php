c8be911ec498f6b721b458cc0de9de9a9a77becd
Yusuf Gündüz <yusuf.gunduz@epias.com.tr>
2018-02-09 11:46:13 +0300

    icon added for the donations admin menu.

--- admin/Donations.php
+++ admin/Donations.php
@@ -10,18 +10,19 @@ class Donations extends PluginBase
         global $wpdb;
         $this->db = &$wpdb;
         $this->tableName = $this->db->prefix . "donations";
-        add_action('admin_menu', array($this, self::PREFIX . "xxxadmin_menu_operations"));
+        add_action('admin_menu', array($this, self::PREFIX . "donation_admin_menu_ops"));
 //        add_action('admin_init', array($this, self::PREFIX . "style_script_reg_operations"));
     }
 
-    public function xxxadmin_menu_operations()
+    public function donation_admin_menu_ops()
     {
         $pageTitle = __("Donations", 'dwc-plugin');
         $menuText = __("Donations", 'dwc-plugin');
         $capability = "manage_options";
         $callback = array($this, self::PREFIX . "donations_page_controller");
         $menuSlug = "donations";
-        add_menu_page($pageTitle, $menuText, $capability, $menuSlug, $callback);
+        $icon = "dashicons-analytics";
+        add_menu_page($pageTitle, $menuText, $capability, $menuSlug, $callback, $icon);
     }
 
     public function donations_page_controller()
@@ -29,18 +30,3 @@ class Donations extends PluginBase
 
     }
 }
-///**
-// * Register a custom menu page.
-// */
-//function wpdocs_register_my_custom_menu_page() {
-//    add_menu_page(
-//        __( 'Custom Menu Title', 'textdomain' ),
-//        'custom menu',
-//        'manage_options',
-//        'myplugin/myplugin-admin.php',
-//        '',
-//        plugins_url( 'myplugin/images/icon.png' ),
-//        6
-//    );
-//}
-//add_action( 'admin_menu', 'wpdocs_register_my_custom_menu_page' );
